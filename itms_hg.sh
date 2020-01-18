1c433dfe1a696f3fb925bc934312526d16ba86ec
duanhw251999 <47199465+duanhw251999@users.noreply.github.com>
2020-01-19 02:21:44 +0800

    Add files via upload

---
+++ itms_hg.sh
@@ -0,0 +1,86 @@
+path=/pardata/EDADATA/INTERFACE/OTHER/DATA/
+path2=/pardata/EDADATA/INTERFACE/OTHER/BACKUP/
+cd  $path
+
+
+readdir() {
+ count=0
+ files=()
+ 
+ for file in `ls Itms_hg_*`
+ do 
+ 	 files[$count]=$file
+ 	 ((count++))
+ done 
+ 
+ msg "scan ${#files[@]} files "
+ 
+ arrsize=${#files[@]}
+ 
+ 
+if [ ${arrsize} > 0 ] ;then
+	for f in ${files[*]}
+	do 
+		execute_fun ${f}
+		`mv ${path}${f} ${path2}`
+	done
+fi
+}
+
+execute_fun(){
+    f=${1}
+    dat=$(convert_dat ${f})
+	verf="i_${f:8:8}_13096_00.verf"
+	echo ${verf}"++++++++"
+	name=${dat}
+	size=$(getSize ${path}${f})
+	rows=`sed -n '$=' ${path}${f}`
+	datestr=${f:8:8}
+	write2verf ${name} ${size} ${rows} ${datestr} ${verf}
+}
+
+convert_dat(){
+  file0=${1}
+  dat_name="i_${file0:8:8}_13096_00_001.dat"
+  `cp ${path}${file0} ${path}${dat_name}`
+  echo ${dat_name}
+}
+
+convert_verf(){
+   file0=${1}
+   verf_name="i_${file0:8:8}_13096_00.verf"
+   `touch ${path}$verf_name`
+   echo ${path}${verf_name}
+}
+
+getSize(){
+  sizestr=$(du -b ${1})
+  arr=(${sizestr// /})
+  size=${arr[0]}
+  echo "${size}"
+  return $size
+}
+
+write2verf(){
+    cur_dateTime0="`date +%Y%m%d%H%m%s`"  
+	name=`printf '%-40s' ${1}`
+	size=`printf '%-20s' ${2}`
+	rows=`printf '%-20s' ${3}`
+	timestamp=`printf '%-20s' ${4}${cur_dateTime0}`
+	echo ${name}${size}${rows}${timestamp} > ${5}
+}
+
+msg(){
+  cur_dateTime="`date +%Y%m%d,%H:%m:%s`"  
+  PID=$$
+  echo "[${cur_dateTime}] $PID " ${1}
+}
+
+#execute block
+while 
+do
+	msg "Start..."
+	readdir
+	msg "End..."
+	sleep 1800
+done
